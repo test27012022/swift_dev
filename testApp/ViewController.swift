01ed6d8553632d478c7d41f4ee0862e4ed4a1e3f
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-02-22 22:22:09 +0300

    commit
    add new project

---
+++ testApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Dmitry P on 21.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hello()
+    }
+    
+    private func hello() {
+        print("hello world")
+    }
+
+}
+
