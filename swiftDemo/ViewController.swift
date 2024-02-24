13c1b337fdff381caf365c3bb2ff5f20b120c414
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-02-24 13:39:51 +0300

    commit initially

---
+++ swiftDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  swiftDemo
+//
+//  Created by Dmitry P on 24.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    private func hello() {
+        print( "say hello")
+    }
+
+}
+
