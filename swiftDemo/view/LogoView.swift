a8b87c7ea5d810490df904312a23ed958c494278
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-03-03 21:04:32 +0300

    commit
    add logo view details

--- swiftDemo/view/LogoView.swift
+++ swiftDemo/view/LogoView.swift
@@ -7,6 +7,53 @@
 
 import UIKit
 class LogoView: UIView {
+    
+    private let imageView: UIImageView = {
+        let view = UIImageView(image: .init(named: "icCalculatorBW"))
+//        let view = UIImageView(image: UIImage(systemName: "backward.end"))
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+            string: "Mr TIP",
+            attributes:[.font: ThemeFont.demiBold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3))
+        label.attributedText = text
+        label.backgroundColor = .red
+        return label
+    }()
+    
+    private let bottomLabel: UILabel = {
+        LabelFactory.buildLabel(text: "Calculator", 
+                                font: ThemeFont.demiBold(ofSize: 20),
+                                backgroundColor: .blue, textAlignment: .left)
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        view.axis = .vertical
+        view.spacing = -4
+        return view
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            imageView,
+            vStackView
+        ])
+        view.axis = .horizontal
+        view.spacing = 8
+        view.alignment = .center
+        return view
+    }()
+    
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -15,8 +62,21 @@ class LogoView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     private func layout() {
-        backgroundColor = .red
+        backgroundColor = .lightGray
+        addSubview(hStackView)
+        
+        hStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(imageView.snp.width)
+           
+        }
+        
+        
     }
     
 }
