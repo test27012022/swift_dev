ca55bb179e4c7eeea555964069ddc25cf93e71a3
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-03-22 21:04:54 +0300

    commit
    setup unit tests
    1.
    -$100 bill
    -no tip
    -1 person

--- swiftDemoTests/swiftDemoTests.swift
+++ swiftDemoTests/swiftDemoTests.swift
@@ -6,30 +6,54 @@
 //
 
 import XCTest
+import Combine
+@testable import swiftDemo
 
 final class swiftDemoTests: XCTestCase {
+    
+    //sut - System Under Test
+    private var sut: CalculatorVM!
+    private var cancellables: Set<AnyCancellable>!
+    
+    private let logoViewTapSubject = PassthroughSubject<Void, Never>()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        sut = .init()
+        cancellables = .init()
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+        cancellables = nil
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testResultWithoutTipForOnePerson() {
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 1
+        
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        // when
+        let output = sut.transform(input: input)
+        // then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 100)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorVM.Input {
+        return.init(
+            billPublisher: Just(bill).eraseToAnyPublisher(),
+            tipPublisher: Just(tip).eraseToAnyPublisher(),
+            splitPublisher: Just(split).eraseToAnyPublisher(),
+            logoViewTapPublisher: logoViewTapSubject.eraseToAnyPublisher())
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
