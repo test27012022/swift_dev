c705d8ed9ed536ed44a320e5720171d1336886d9
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-03-27 21:25:10 +0300

    commit
    add all screen elements

--- swiftDemo/model/ScreenIdentifier.swift
+++ swiftDemo/model/ScreenIdentifier.swift
@@ -28,6 +28,7 @@ enum ScreenIdentifier {
         case fifteenPercentButton
         case twentyPercentButton
         case customTipButton
+        case customTipAlertTextField
     }
     
     enum SplitInputView: String {
